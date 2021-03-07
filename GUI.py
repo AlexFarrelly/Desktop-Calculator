d22309df373abcd588dd7e2b2871acc12832ead2
Alex Farrelly <78617746+AlexFarrelly@users.noreply.github.com>
2021-03-07 23:28:08 +0000

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -1,26 +1,26 @@
 from tkinter import *
+from tkinter import font
 import tkinter as tk
 import functions as func
 
 
-class window():
+class Matrixwindow(tk.Frame):
     def __init__(self):
         GUI = tk.Tk()
-        GUI.title(""), GUI.config(bg = "#171717")
-        self.DisplayWindow(GUI)
-        self.SpecialButtons(GUI)
-        self.MainButtonFrame(GUI)
-        GUI.mainloop()
+        GUI.title("A-level Calculator"), GUI.config(bg = "#171717")
+        self.DisplayWindow(GUI) and self.ButtonsFrame(GUI)
     
     class DisplayWindow():
         def __init__(self, GUI):
+            super().__init__()
+            self.GUI = GUI
             self.inputlist= []
             topFrame = tk.Frame(GUI)
             self.inputLabel = tk.Label(topFrame, text = "", width = 40, height = 4, padx = 6, anchor = "w")
             self.inputLabel.grid(row = 0, column = 0)
-            self.anwserLabel = tk.Label(topFrame, width = 40, height = 4, padx = 6, anchor = "e")
+            self.anwserLabel = tk.Label(topFrame, text = 0, width = 40, height = 4, padx = 6, anchor = "e")
             self.anwserLabel.grid(row = 1, column = 0)
-            topFrame.grid(row = 0, column = 0)
+            topFrame.grid(row = 0, column = 0, padx = 20, pady = 10)
 
         def enteredValue(self, text):
             if text == None:
@@ -38,41 +38,70 @@ class window():
 
         def clearValues(self):
             self.inputlist.clear()
-            self.anwserLabel.config(text = "")
+            self.anwserLabel.config(text = 0)
             self.enteredValue(None)
 
-        def exuecute(self):
-            try:
+        def sdValues(self, value):
+            if self.state == False:
+                a = func.rationalAprox(str(value))
+                self.anwserLabel.configure(text = (a.anwser))
+                self.state = True 
+            else:
+                self.anwserLabel.configure(text = self.lastanwser)
+                self.state = False
+
+        def exuecute(self, containsSpecialFunction):
+            if containsSpecialFunction == False:
                 try:
-                    a = func.basicFunctions(self.inputlist, 0)
-                    while a.done == False:
-                        a = func.basicFunctions(a.newvalues, 0)
-                    anws = a.anwser
-                    self.anwserLabel.config(text = anws)
+                    brackets = func.priorityValues(self.inputlist)
+                    values = brackets.newEquation
+                    self.inputlist = values
+                    anws = [str(i) for i in values]
+                    anws = "".join(anws)
+                    anws = float(anws)
+                    if anws % 1 == 0:
+                        anws == int(anws)
+                    else:
+                        anws = float(anws)
                     self.lastanwser = anws
-                    self.inputlist.clear()
+                    self.anwserLabel.config(text = anws)
                 except:
-                    self.anwserLabel.config(text = self.inputLabel.cget("text"))
-                    self.inputlist.clear()
-            except:
-               print(2)
+                    try:
+                        try:
+                            a = func.basicFunctions(self.inputlist, 0)
+                            while a.done == False:
+                                a = func.basicFunctions(a.newvalues, 0)
+                            anws = a.anwser
+                            self.anwserLabel.config(text = anws)
+                            self.lastanwser = anws
+                        except:
+                            anws = [str(i) for i in self.inputLabel.cget("text")]
+                            anws = "".join(anws)
+                            if float(anws) % 1 == 0:
+                                anws == int(anws)
+                            else:
+                                anws = float(anws)
+                            self.lastanwser = anws
+                            self.anwserLabel.config(text = anws)
+                    except:
+                       ErrorWindow("Syntax Error")
+            else:
+                pass
 
-        def sdValues(self):
-            print(self.inputlist)
-            a = self.inputLabel["text"]
-            print(a)
-            a = func.fractions(a)
 
-    class SpecialButtons(DisplayWindow):
+    class ButtonsFrame(DisplayWindow):
         def __init__(self, GUI):
             super().__init__(GUI)
-            self.inputList = []
+            self.inputList, self.state, containsSpecialFunction = [], False, False
             middleFrame = tk.Frame(GUI)
+            
+##          Special Buttons-----------------------------------------------------------------------------------------
             optionButton, varsButton, menuButton, squareButton, powerButton, exitButton = tk.Button(middleFrame), tk.Button(middleFrame), tk.Button(middleFrame), tk.Button(middleFrame), tk.Button(middleFrame), tk.Button(middleFrame)
             varibleButton, logButton, naturalLogButton, sinButton, cosButton, tanButton = tk.Button(middleFrame), tk.Button(middleFrame), tk.Button(middleFrame), tk.Button(middleFrame), tk.Button(middleFrame), tk.Button(middleFrame)
             fractionButton, sdButton, outsideBracketButton, insideBracketButton, commaButton, arrowButton = tk.Button(middleFrame), tk.Button(middleFrame), tk.Button(middleFrame), tk.Button(middleFrame), tk.Button(middleFrame), tk.Button(middleFrame)
             buttonText, textValue = ["OPTN", "VARS", "MENU", "x^2", "^", "EXIT", "X,θ,T", "log", "ln", "sin", "cos", "tan", "[]/[]", "S<->D", "(", ")", ",", "->"], 0
             rowPose, columnPose = 0, 0
+            
             for i in optionButton, varsButton, menuButton, squareButton, powerButton, exitButton, varibleButton, logButton, naturalLogButton, sinButton, cosButton, tanButton, fractionButton, sdButton, outsideBracketButton, insideBracketButton, commaButton, arrowButton:
                 if i == squareButton:
                     rowPose, columnPose = 1, 0
@@ -80,22 +109,22 @@ class window():
                     rowPose, columnPose = 2, 0
                 elif i == fractionButton:
                     rowPose, columnPose = 3, 0
-                i.config(text = buttonText[textValue], width = 5, height = 1, padx = (33/10))
+                i.config(text = buttonText[textValue], width = 5, height = 1, padx = 4)
                 i.grid(row = rowPose, column = columnPose)
                 columnPose += 1
                 textValue += 1
-            sdButton.config(command = self.sdValues)
-            middleFrame.grid(row = 1, column = 0)
+            sdButton.config(command = lambda: self.sdValues(self.lastanwser)), outsideBracketButton.config(command = lambda: self.enteredValue(outsideBracketButton.cget("text"))), insideBracketButton.config(command = lambda: self.enteredValue(insideBracketButton.cget("text")))
+            squareButton.config(command = lambda: (self.enteredValue("^"), self.enteredValue("2"))), powerButton.config(command = lambda: (self.enteredValue("^"))), sinButton.config(command = lambda: (self.enteredValue("sin")))
+            cosButton.config(command = lambda: (self.enteredValue("cos"))), tanButton.config(command = lambda: (self.enteredValue("tan")))
+            middleFrame.grid(row = 1, column = 0, padx = 20, pady = 10)
+            bottomFrame = tk.Frame(GUI)
             
-
-    class MainButtonFrame(DisplayWindow):
-        def __init__(self, GUI):
-            super().__init__(GUI)
-            bottomFrame = tk.Frame(GUI, bg = "white")
-            #Number Buttons------------------------------------------------------------------------------------------
+##          Number Buttons------------------------------------------------------------------------------------------
             number, columnPose, rowPose = 1, 0, 2
             oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton, sevenButton, eightButton, nineButton = tk.Button(bottomFrame), tk.Button(bottomFrame), tk.Button(bottomFrame), tk.Button(bottomFrame), tk.Button(bottomFrame), tk.Button(bottomFrame),tk.Button(bottomFrame), tk.Button(bottomFrame), tk.Button(bottomFrame)
             buttons = [oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton, sevenButton, eightButton, nineButton]
+            
+##          This Creates all the grid configs for all the buttons in the main button frame----------------------------------------------------------------- 
             for i in oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton, sevenButton, eightButton, nineButton:
                 if i == fourButton:
                     rowPose, columnPose = 1, 0
@@ -105,17 +134,22 @@ class window():
                 i.grid(row = rowPose, column = columnPose)
                 number += 1
                 columnPose += 1
+
+##          Commands for all the buttons in the main button frame----------------------------------------------------
             oneButton.config(command = lambda: self.enteredValue(oneButton.cget("text"))), twoButton.config(command = lambda: self.enteredValue(twoButton.cget("text"))), threeButton.config(command = lambda: self.enteredValue(threeButton.cget("text")))
             fourButton.config(command = lambda: self.enteredValue(fourButton.cget("text"))), fiveButton.config(command = lambda: self.enteredValue(fiveButton.cget("text"))), sixButton.config(command = lambda: self.enteredValue(sixButton.cget("text")))
             sevenButton.config(command = lambda: self.enteredValue(sevenButton.cget("text"))), eightButton.config(command = lambda: self.enteredValue(eightButton.cget("text"))), nineButton.config(command = lambda: self.enteredValue(nineButton.cget("text")))
             zeroButton = tk.Button(bottomFrame, text = 0, width = 7, height = 1, command = lambda: self.enteredValue(zeroButton.cget("text")))
             zeroButton.grid(row = 3,column = 0)
             columnPose, rowPose = 3, 0
-            #Function Buttons-----------------------------------------------------------------------------------------
+            
+##          Function Buttons-----------------------------------------------------------------------------------------
             buttonText, textValue = ["x", "÷", "+", "-", "Ans", "x10^x"], 0
             deleteButton, ACbutton, multiplyButton, divideButton, addButton, minusButton, ansButton, executeButton = tk.Button(bottomFrame), tk.Button(bottomFrame), tk.Button(bottomFrame), tk.Button(bottomFrame), tk.Button(bottomFrame), tk.Button(bottomFrame),tk.Button(bottomFrame), tk.Button(bottomFrame)
             deleteButton.config(text = "DEL",width = 7, height = 1, bg = "blue"), ACbutton.config(text = "AC",width = 7, height = 1, bg = "blue"), executeButton.config(text = "EXE",width = 7, height = 1, bg = "blue")
             deleteButton.grid(row = 0, column = 3), ACbutton.grid(row = 0, column = 4), executeButton.grid(row = 3, column = 4)
+
+##          This Creates all the grid configs for all the buttons in the function button frame-------------------------------
             for i in multiplyButton, divideButton, addButton, minusButton, ansButton:
                 if i == multiplyButton:
                     rowPose, columnPose = 1, 3
@@ -127,19 +161,39 @@ class window():
                 i.grid(row = rowPose, column = columnPose)
                 textValue += 1
                 columnPose += 1
+                
             multiplyButton.config(command = lambda: self.enteredValue(multiplyButton.cget("text"))), divideButton.config(command = lambda: self.enteredValue(divideButton.cget("text")))
             addButton.config(command = lambda: self.enteredValue(addButton.cget("text"))), minusButton.config(command = lambda: self.enteredValue(minusButton.cget("text")))
             ansButton.config(command = lambda: self.enteredValue(self.lastanwser)),deleteButton.config(command = self.deleteValue), ACbutton.config(command = self.clearValues)
-            executeButton.config(command = self.exuecute)
-            #Special Buttons-------------------------------------------------------------------------------------------
+            executeButton.config(command = lambda: self.exuecute(containsSpecialFunction))
+            
+##          Special Buttons-------------------------------------------------------------------------------------------
             decimalButton = tk.Button(bottomFrame, text = ".", width = 7, height = 1, command = lambda: self.enteredValue(decimalButton.cget("text")))
             decimalButton.grid(row = 3,column = 1)
-            timesTenButton = tk.Button(bottomFrame, text = "x10^x", width = 7, height = 1, command = lambda: self.enteredValue("x10^"))
+            timesTenButton = tk.Button(bottomFrame, text = "x10^x", width = 7, height = 1, command = lambda: (self.enteredValue("x"), self.enteredValue("10"), self.enteredValue("^")))
             timesTenButton.grid(row = 3,column = 2)            
-            bottomFrame.grid(row = 2, column = 0)
+            bottomFrame.grid(row = 2, column = 0, padx = 20, pady = 10)
+
+class ErrorWindow():
+    def __init__(self, error):
+        errorFont = font.Font(family="Helvetica",size=36,weight="bold")
+        ErrorUI = tk.Toplevel()
+        ErrorUI.title("ERROR")
+        Message = tk.Label(ErrorUI,text= error, font = errorFont).grid(row = 1, column = 1,)
+        Button = tk.Button(ErrorUI,text = "Ok", command = lambda: ErrorUI.destroy()).grid(row=2, column = 1)
         
-window()
+        
 
 
 
 
+
+
+
+
+
+
+
+        
+        
+Matrixwindow()
--- functions.py
+++ functions.py
@@ -42,58 +42,143 @@ class basicFunctions():
     def method(self, a, b, func):
         if func == ["x"]:
             self.anwser += a * b
-        elif func == ["÷"]:
+        elif func == ["÷"] or func == ["/"]:
             self.anwser = a / b
         elif func == ["+"]:
             self.anwser += a + b
         elif func == ["-"]:
-            self.anwser += a - b
+            self.anwser += a - b 
         if self.anwser % 1 == 0:
             self.anwser = int(self.anwser)
-            return self.anwser
         else:
-            return self.anwser
-
-class fractions():
+            try:
+                self.anwser = (round(self.anwser, 12))
+            except:
+                pass
+        return self.anwser
+            
+class priorityValues():
     def __init__(self, values):
-        self.rationalAprox(values)
-    class rationalAprox():
-        def __init__(self, value):
-            try:
-                self.decimal("68.975")
-            except:
-                print("Syntax Error")
+        bracketsValues, removedValues, self.newEquation = [], [], []
+        self.searchForIndices(values, removedValues)
+        self.searchForBrackets(self.newEquation, removedValues, bracketsValues)
+        for i in self.newEquation:
+            if i == "^" or i == "(":
+                print(self.newEquation)
+                priorityValues(self.newEquation)
 
-        def decimal(self, n):
-            wholeN, count = [], 0
-            try:
-                listN = list(n)
-                for i in listN:
-                    if i == ".":
-                        for i in range(count):
-                            wholeN.append(listN[0])
-                            listN.pop(0)
-                        listN, wholeN = [str(i) for i in listN], [str(a) for a in wholeN]
-                        listN, wholeN = "".join(listN), "".join(wholeN)
-                        listN, wholeN = float(listN), int(wholeN)
-                        return self.mediant(listN, wholeN, 0, 1, 1, 1)
-                    else:
-                        count += 1
-                print(n)
-            except:
-                return self.mediant(float(n), 0, 0, 1, 1, 1)
-
-        def mediant(self, n, wn, a, b, c, d):
-            try:
-                mediantTop = a+c
-                mediantBottom = b + d
-                if n > (mediantTop/mediantBottom):
-                    self.mediant(n, wn, mediantTop, mediantBottom, c, d)
-                elif n < (mediantTop/mediantBottom):
-                    self.mediant(n, wn ,a, b, mediantTop, mediantBottom) 
+    def searchForBrackets(self, values, removedValues, bracketsValues):
+        try:
+            for i in range(len(values)):
+                if values[i] == "(":
+                    removedValues.append(i)
+                    for var in range((i+1), len(values)):
+                        if values[var] == ")":
+                            removedValues.append(var)
+                            break
+                        else:
+                            bracketsValues.append(values[var])
+                            removedValues.append(var)
+                    break
+            if len(bracketsValues) > 0:
+                for i in range(len(removedValues)):
+                    values.pop(removedValues[0])
+                bracketAnwser = basicFunctions(bracketsValues, 0)
+                
+                while bracketAnwser.done == False:
+                    bracketAnwser = basicFunctions(bracketAnwser.newvalues, 0)
+                bracketAnwser = bracketAnwser.anwser
+                if len(removedValues) > 0:
+                    self.newEquation.insert(removedValues[0], bracketAnwser)
+            else:
+                self.newEquation = values
+        except:
+            pass
+            
+    def searchForIndices(self, values, removedValues):
+        try:
+            newleft, self.newEquation, leftValue, rightValue = 1,[], [], []
+            for i in range(len(values)):
+                if values[i] == "^":
+                    removedValues.append(i)
+                    for var in range(i+1, len(values)):
+                        if values[var] == "+" or values[var] =="x" or values[var] =="-" or values[var] =="÷" or values[var] == ")":
+                            break
+                        else:
+                            rightValue.append(values[var])
+                            removedValues.append(var)
+            i = (removedValues[0]-1)
+            while i >= 0:
+                if values[i] == "+" or values[i] =="x" or values[i] =="-" or values[i] =="÷" or values[i] == "(":
+                    break
                 else:
-                    return print((mediantTop + (wn * mediantBottom)), "/", mediantBottom)
+                    if i == 0:
+                        leftValue.append(values[i])
+                        removedValues.append(i)
+                        break
+                    else:
+                        leftValue.append(values[i])
+                        removedValues.append(i)
+                        i -= 1
+            if len(removedValues) > 0:
+                left, right = [str(num) for num in reversed(leftValue)], [str(num) for num in rightValue]
+                left, right  = "".join(left), "".join(right)
+                left, right  = float(left), int(right)
+                for i in range(right):
+                    newleft *= left
+            try:
+                if len(removedValues) != len(values):
+                    for i in range(len(removedValues)):
+                        values.pop(removedValues[(len(removedValues)-1)])
+                    self.newEquation = values
+                    pose = removedValues[len(removedValues)-1]
+                    self.newEquation.insert(pose, newleft)
+                    self.searchForBrackets(self.newEquation, [], [])
+                    
+                else:
+                    self.newEquation.append((round(newleft, 12)))
             except:
-                return print(n)
-    
+                pass
+        except:
+            self.newEquation = values
 
+
+class rationalAprox():
+    def __init__(self, value):
+        self.anwser = 0
+        try:
+            self.decimal(value)
+        except:
+            print("Syntax Error")
+        
+
+    def decimal(self, n):
+        wholeN, count = [], 0
+        try:
+            listN = list(n)
+            for i in listN:
+                if i == ".":
+                    for i in range(count):
+                        wholeN.append(listN[0])
+                        listN.pop(0)
+                    listN, wholeN = [str(i) for i in listN], [str(a) for a in wholeN]
+                    listN, wholeN = "".join(listN), "".join(wholeN)
+                    listN, wholeN = float(listN), int(wholeN)
+                    return self.mediant(listN, wholeN, 0, 1, 1, 1)
+                else:
+                    count += 1
+        except:
+            return self.mediant(float(n), 0, 0, 1, 1, 1)
+
+    def mediant(self, n, wn, a, b, c, d):
+        try:
+            mediantTop = a+c
+            mediantBottom = b + d
+            if n > (mediantTop/mediantBottom):
+                self.mediant(n, wn, mediantTop, mediantBottom, c, d)
+            elif n < (mediantTop/mediantBottom):
+                self.mediant(n, wn ,a, b, mediantTop, mediantBottom) 
+            else:
+                self.anwser = (mediantTop + (wn * mediantBottom), "/", mediantBottom)
+        except:
+            pass
